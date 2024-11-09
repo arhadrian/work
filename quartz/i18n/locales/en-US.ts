be83fab9ebc1f1385ceff3318374d83e1eadd79d
arhadrian <adrian.vidrascu@gmail.com>
2024-11-09 23:02:17 +0200

    Quartz sync: Nov 9, 2024, 11:02 PM

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -30,7 +30,7 @@ export default {
       darkMode: "Dark mode",
     },
     explorer: {
-      title: "Explorer",
+      title: "Componente",
     },
     footer: {
       createdWith: "Created with",
